0e05c8a66094ae7358a65dedb090e58f760889c4
adam-dev2 <shaikadam252@gmail.com>
2025-01-15 16:43:53 +0530

    reached interfaces in TS

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+// let x: string | null = prompt('Enter firstname: ');
+
+// console.log('hello',x);
+
+// function legal(age: number){
+//     if(age>18) return true
+//     return false;
+// }
+
+// let ans:boolean = legal(21);
+
+// console.log(ans)
+
+// function delayedcall (fn:()=>void){
+//     setTimeout(fn,1000);
+// }
+
+// function log() {
+//     console.log('asd');
+// }
+
+// delayedcall(log);
+
+// function greet(user:{
+//     name: string,
+//     age:number
+// }) {
+//     console.log("hello",user.name);
+// }
+
+// const user = {
+//     name : 'adam',
+//     age:22
+// }
+
+// greet(user);
+
+// Interfaces
